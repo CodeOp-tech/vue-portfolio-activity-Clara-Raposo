241dee880702e74bafebd750ea31e443a5d90590
Clara-Raposo-Sanchez <clara.ths@gmail.com>
2023-06-11 12:26:09 +0200

    Estilos componente detalle projecto

--- src/components/ProjectDetail.vue
+++ src/components/ProjectDetail.vue
@@ -1,10 +1,14 @@
 <template>
     <div class = "detail-project">
-      <article v-for="project in projects" :key="project.title" :project="project">
-        <img :src= "project.image" :alt="project.title" class="grid-container__image">
-        <h2>{{ project.title }}</h2>
-        <p>{{ project.description }}</p>
-      </article>
+    
+        <div class = "detail-project-image-container">
+          <img src= "https://picsum.photos/536/354" class="detail-project__image">
+        </div>
+        <div class = "detail-project-information">
+          <h2>This is my project</h2>
+          <p>The description of my project to be presented in the main page.</p>
+        </div>
+      
     </div>
   </template>
   
@@ -13,14 +17,13 @@
     name: "ProjectDetail",
     data() {
         return {
-        isAdmin: true, //verifica si estás dentro de la pestaña de Admin
-        allProjects: [] //array donde se guardan los proyectos
+        ProjectInfo: [] 
         };
     },
     methods: {
-        addProject(project) {
+      addProject(project) {
         this.allProjects.push(project);
-        }
+      }
 
     }
   };
@@ -33,7 +36,32 @@
     padding: 20px;
     float: right;
     margin-bottom: 20px;
+    display: flex;
+    gap: 25px;
   }
-  
+  .detail-project-image-container{
+    height: 400px;
+    display: flex;
+  }
+  .detail-project__image{
+    width: 100%;
+    aspect-ratio: 1;
+    object-fit: cover;
+  }
+  .detail-project-information{
+    display: block;
+  }
+  .detail-project-information > h2,p{
+    text-transform: uppercase;
+  }
+  .detail-project-information > h2{
+    letter-spacing: 1px;
+    word-spacing: 3px;
+    background-color: pink;
+    padding: 8px 15px;
+    margin: 0px;
+    width: 59%;
+    text-align: left;
+  }
   </style>
   
